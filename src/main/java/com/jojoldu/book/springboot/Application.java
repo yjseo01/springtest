738cb03da5fc19feee4cf386ccb08d6e21e0d70c
yjseo01 <yujin071610@gmail.com>
2024-03-12 23:03:58 +0900

    ch5

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @EnableJpaAuditing // JPA Auditing 활성화
-//@SpringBootApplication // 여기부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트에 최상단에 위치
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication // 여기부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트에 최상단에 위치
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application { // 앞으로 만들 프로젝트의 메인 클래스
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); // 내장 WAS 실행 -> 톰캣 설치 x, Jar파일로 실행
---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -0,0 +1,56 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String GetRolekey() {
+        return this.role.getKey();
+    }
+
+}
